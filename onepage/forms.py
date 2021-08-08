21b587dbb65eefa932246bc5a2e0f74e5ffa12a3
Nj Nafir <mdanjenafir24434@gmail.com>
2021-08-08 18:31:02 +0600

    new feature onepage.forms.ModelForm and onepage.forms.Form

---
+++ onepage/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class ModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+        bootstrap_visible_fields(self.visible_fields)
+
+
+class Form(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(Form, self).__init__(*args, **kwargs)
+        bootstrap_visible_fields(self.visible_fields)
